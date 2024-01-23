c7634448a129276b9288718be63d7ff1a18484b1
Levi Peeters <levipeeters1@hotmail.com>
2024-01-23 14:54:51 +0100

    Progress on filling out functions

--- Rule.py
+++ Rule.py
@@ -6,21 +6,74 @@ import utils_readable
 import Beam
 import RuleGrowConstraint
 import DataInfo
+import Ruleset
 
 import constant
+
+def store_grow_info(excl_bi_array, incl_bi_array, icol, cut, cut_option, excl_mdl_gain, incl_mdl_gain,
+                    coverage_excl, coverage_incl, normalized_gain_excl, normalized_gain_incl, _rule):
+    """ Store the information of a grow step in a dictionary."""
+    
+    excl_coverage = np.count_nonzero(excl_bi_array)
+    incl_coverage = np.count_nonzero(incl_bi_array)
+    
+    # locals() returns a dictionary containing all local variables
+    return locals()
+
+def store_grow_info_rulelist(excl_bi_array, icol, cut, cut_option, excl_normalized_gain):
+    return {"excl_bi_array": excl_bi_array, "icol": icol, "cut": cut,
+            "cut_option": cut_option, "excl_normalized_gain": excl_normalized_gain}
 
 class Rule:
     def __init__(self, 
                  indices, 
-                 indices_excl_overlap, 
+                 indices_excl, 
                  data_info: DataInfo.DataInfo,
                  rule_base, 
                  condition_matrix, 
-                 ruleset,
-                 excl_mdl_gain, 
-                 incl_mdl_gain, 
+                 ruleset: Ruleset.Ruleset,
+                 mdl_gain, 
+                 mdl_gain_excl, 
                  icols_in_order):
-        pass
+        
+        self.ruleset = ruleset
+        self.data_info = data_info # Metadata
+        self.rule_base = rule_base  # The previous level of this rule
+        self.icols_in_order = icols_in_order  # The order of the columns in the data
+
+        # Numpy arrays with indices of covered indices
+        self.indices = indices  
+        self.indices_excl = indices_excl  
+
+        # Boolean arrays indicating covered indices
+        self.bool_array = self.get_bool_array(self.indices)
+        self.bool_array_excl = self.get_bool_array(self.indices_excl)
+
+        # Number of instances covered by this rule
+        self.coverage = len(self.indices) 
+        self.coverage_excl = len(self.indices_excl)
+
+        # Feature- and target subvector covered by the rule
+        self.features = self.data_info.features[indices]
+        self.target = self.data_info.target[indices]
+        self.features_excl = self.data_info.features[indices_excl]
+        self.target_excl = self.data_info.target[indices_excl]
+
+        # Condition matrix containing the rule literals and a boolean array to show which features have a condition
+        self.condition_matrix = condition_matrix
+        self.condition_bool = (~np.isnan(condition_matrix[0])).astype(int) + (~np.isnan(condition_matrix[1])).astype(int)
+
+        # Probabilities and regrets
+        self.prob_excl = self._calc_probs(target=self.target_excl_overlap)
+        self.prob = self._calc_probs(target=self.target)
+        self.regret_excl = nml_regret.regret(self.nrow_excl, data_info.num_class)
+        self.regret = nml_regret.regret(self.nrow, data_info.num_class)
+        self.negloglike_excl = utils_calculating_cl.calc_negloglike(p=self.prob_excl, n=len(self.indices_excl))
+        self.cl_model = self.ruleset.model_encoding.rule_cl_model_dep(self.condition_matrix, col_orders=icols_in_order)
+
+        self.mdl_gain = mdl_gain
+        self.mdl_gain_excl = mdl_gain_excl
+
 
     def _calc_probs(self, target):
         """ Given a vector containing the targets of a set of instances, calculate the probability of encountering each target.
@@ -35,7 +88,7 @@ class Rule:
         : Array
             Vector containing the probability of encountering each target
         """
-        pass
+        return utils_calculating_cl.calc_probs(target, num_class=self.data_info.num_class, smoothed=False)
 
     def get_bool_array(self, indices):
         """ Given a list of indices, return a boolean array with True at the indices in the list.
@@ -49,7 +102,9 @@ class Rule:
         bool_array : Array
             Boolean array with True at the indices in the list
         """
-        pass
+        bool_array = np.zeros(self.data_info.nrow, dtype=bool)
+        bool_array[indices] = True
+        return bool_array
 
     def get_candidate_cuts_icol_given_rule(self, candidate_cuts, icol):
         """ Given a list of candidate cuts and an index of a feature, return the candidate cuts for that feature.
@@ -68,8 +123,8 @@ class Rule:
         pass
 
     def update_grow_beam(self, bi_array, excl_bi_array, icol, cut, cut_option, incl_coverage, excl_coverage,
-                         grow_info_beam, grow_info_beam_excl, _validity):
-        """ Use rule growing information to update the beam.
+                         grow_info_beam: Beam.GrowInfoBeam, grow_info_beam_excl: Beam.GrowInfoBeam, _validity):
+        """ Use information of a grow step to update the beam.
 
         Parameters
         ---
@@ -98,22 +153,92 @@ class Rule:
         ---
         None
         """
+        # Calculate the MDL gain
+        info_theo_scores = self.calculate_mdl_gain(bi_array=bi_array, excl_bi_array=excl_bi_array,
+                                                   icol=icol, cut_option=cut_option)
+        
+        # Store info in a dictionary
+        grow_info = store_grow_info(
+            excl_bi_array=excl_bi_array, incl_bi_array=bi_array, icol=icol,
+            cut=cut, cut_option=cut_option, incl_mdl_gain=info_theo_scores["absolute_gain"],
+            excl_mdl_gain=info_theo_scores["absolute_gain_excl"],
+            coverage_excl=excl_coverage, coverage_incl=incl_coverage,
+            normalized_gain_excl=info_theo_scores["absolute_gain_excl"] / excl_coverage,
+            normalized_gain_incl=info_theo_scores["absolute_gain"] / excl_coverage, # Question: Shouldn't this be incl_coverage?    
+            _rule=self
+        )
+
+        # Ratio of coverage after the grow step to the coverage of the rule before the grow step
+        # This is the gain of the grow step
+        excl_cov_percent = grow_info["coverage_excl"] / self.coverage_excl
+        incl_cov_percent = grow_info["coverage_incl"] / self.coverage
+
+        # Update the beams if the grow step is valid
+        if _validity["res_excl"]:
+            grow_info_beam_excl.update(grow_info, grow_info["normalized_gain_excl"], excl_cov_percent)
+
+        if _validity["res_incl"]:
+            grow_info_beam.update(grow_info, grow_info["normalized_gain_incl"], incl_cov_percent)
 
     def grow(self, grow_info_beam, grow_info_beam_excl):
-        """ Grow the rule by one step.
+        """ Grow the rule by one step and update the 
         
         Parameters
         ---
-        grow_info_beam : GrowInfoBeam object
-            Beam of rules with previously covered instances
-        grow_info_beam_excl : GrowInfoBeam object
-            Beam of rules without previously covered instances
+        grow_info_beam : DiverseCovarianceBeam object
+            Beam object to store information for this grow step
+        grow_info_beam_excl : DiverseCovarianceBeam object
+            Beam of object to store information for this grow step without considering previously covered instances
             
         Returns
         ---
         None
         """
-        pass
+        candidate_cuts = self.data_info.candidate_cuts
+
+        # Consider each feature
+        for icol in range(self.data_info.ncol):
+            candidate_cuts_icol = self.get_candidate_cuts_icol_given_rule(candidate_cuts, icol)
+
+            # Consider every candidate cut point
+            for i, cut in enumerate(candidate_cuts_icol):
+                
+                # Check validity and skip if not valid
+                _validity = RuleGrowConstraint.validity_check(rule=self, icol=icol, cut=cut)
+                if self.data_info.not_use_excl_:
+                    _validity["res_excl"] = False
+
+                if _validity["res_excl"] == False and _validity["res_incl"] == False:
+                    continue
+
+                # Construct binary arrays indicating which features fall on each side of the cut
+                excl_left_bi_array = (self.features_excl[:, icol] < cut)
+                excl_right_bi_array = ~excl_left_bi_array
+                left_bi_array = (self.features[:, icol] < cut)
+                right_bi_array = ~left_bi_array
+
+                incl_left_coverage, incl_right_coverage = np.count_nonzero(left_bi_array), np.count_nonzero(
+                    right_bi_array)
+                excl_left_coverage, excl_right_coverage = np.count_nonzero(excl_left_bi_array), np.count_nonzero(
+                    excl_right_bi_array)
+
+                # QUESTION: Why is there no check on incl_coverage being 0?
+                if excl_left_coverage == 0 or excl_right_coverage == 0:
+                    continue
+
+                # Update the beam with the results. We do this twice, because a cut can be < or >
+                self.update_grow_beam(bi_array=left_bi_array, excl_bi_array=excl_left_bi_array, icol=icol,
+                                      cut=cut, cut_option=constant.LEFT_CUT,
+                                      incl_coverage=incl_left_coverage, excl_coverage=excl_left_coverage,
+                                      grow_info_beam=grow_info_beam, grow_info_beam_excl=grow_info_beam_excl,
+                                      _validity=_validity)
+
+                self.update_grow_beam(bi_array=right_bi_array, excl_bi_array=excl_right_bi_array, icol=icol,
+                                      cut=cut, cut_option=constant.RIGHT_CUT,
+                                      incl_coverage=incl_right_coverage, excl_coverage=excl_right_coverage,
+                                      grow_info_beam=grow_info_beam, grow_info_beam_excl=grow_info_beam_excl,
+                                      _validity=_validity)
+
 
     def calculate_mdl_gain(self, bi_array, excl_bi_array, icol, cut_option):
         """ Calculate the MDL gain when adding a cut to the rule.
@@ -134,7 +259,18 @@ class Rule:
           : dict
             Containing codelengths and gains for this split
         """
-        pass
+        data_encoding, model_encoding = self.ruleset.data_encoding, self.ruleset.model_encoding
+
+        cl_model = model_encoding.cl_model_after_growing_rule_on_icol(rule=self, ruleset=self.ruleset, icol=icol,
+                                                                      cut_option=cut_option)
+        cl_data = data_encoding.get_cl_data_incl(self.ruleset, self, excl_bi_array=excl_bi_array, incl_bi_array=bi_array)
+        cl_data_excl = data_encoding.get_cl_data_excl(self.ruleset, self, excl_bi_array)
+
+        absolute_gain = self.ruleset.total_cl - cl_data - cl_model
+        absolute_gain_excl = self.ruleset.total_cl - cl_data_excl - cl_model
+
+        return {"cl_model": cl_model, "cl_data": cl_data, "cl_data_excl": cl_data_excl,
+                "absolute_gain": absolute_gain, "absolute_gain_excl": absolute_gain_excl}
 
     def _print(self):
         """ Print the rule.
--- Ruleset.py
+++ Ruleset.py
@@ -11,10 +11,62 @@ import ModelEncoding
 import DataEncoding
 
 def make_rule_from_grow_info(grow_info):
-    pass
+    """ Make a rule from the information of a grow step
+
+    Parameters
+    ---
+    grow_info : Dict
+        Information about the grow step
+    
+    Returns
+    ---
+    rule : Rule object
+        Rule created from the grow step information
+    """
+
+    rule = grow_info["_rule"]
+    indices = rule.indices[grow_info["incl_bi_array"]]
+    indices_excl_overlap = rule.indices_excl_overlap[grow_info["excl_bi_array"]]
+
+    condition_matrix = np.array(rule.condition_matrix)
+    condition_matrix[grow_info["cut_option"], grow_info["icol"]] = grow_info["cut"]
+    if grow_info["icol"] in rule.icols_in_order:
+        new_icols_in_order = rule.icols_in_order
+    else:
+        new_icols_in_order = rule.icols_in_order + [grow_info["icol"]]
+    rule = Rule(indices=indices, indices_excl_overlap=indices_excl_overlap, data_info=rule.data_info,
+                rule_base=rule, condition_matrix=condition_matrix, ruleset=rule.ruleset,
+                excl_mdl_gain=grow_info["excl_mdl_gain"],
+                incl_mdl_gain=grow_info["incl_mdl_gain"],
+                icols_in_order=new_icols_in_order)
+    return rule
 
 def extract_rules_from_beams(beams):
-    pass
+    ''' Extract rules from beams
+
+    Parameters
+    ---
+    beams : list of Beam objects
+        List of beams from which the rules are extracted
+    
+    Returns
+    ---
+    rules : list of Rule objects
+    '''
+    rules = []
+    coverage_list = []
+    for beam in beams:
+        for info in beam.infos:
+            if info["coverage_incl"] in coverage_list:
+                index_equal = coverage_list.index(info["coverage_incl"])
+                if np.all(rules[index_equal].indices == info["_rule"].indices[info["incl_bi_array"]]):
+                    continue
+
+            r = make_rule_from_grow_info(grow_info=info)
+            rules.append(r)
+            coverage_list.append(r.coverage)
+    return rules
+
 
 class Ruleset:
     def __init__(self,
@@ -95,6 +147,7 @@ class Ruleset:
 
     def fit(self, max_iter=1000, printing=True):
         """ Fit the data by iteratively adding rules to the ruleset
+        # TODO: Think about logging
         
         Parameters
         ---
@@ -108,8 +161,19 @@ class Ruleset:
         total_cl : float
             Total code length of the ruleset
         """
+
+        # Keep track of CL progression
+        total_cl = [self.total_cl]
+
+        for iter in range(max_iter):
+            if printing:
+                print("Iteration: ", iter)
+            
+            # Grow a rule
+            rule_to_add = self.search_next_rule(k_consecutively=5)
+            
+
         
-        pass
 
     @staticmethod
     def calculate_stop_condition_element(incl_beam, excl_beam, prev_best_gain, prev_best_excl_gain):
@@ -131,10 +195,17 @@ class Ruleset:
         : bool
             Whether the stop condition is met
         """
-        pass
+        # For incl and excl beam, check if the max gain is lower than the previous best gain
+        condition1 = len(incl_beam.gains) > 0 and np.max(incl_beam.gains) < prev_best_gain
+        condition2 = len(excl_beam.gains) > 0 and np.max(excl_beam.gains) < prev_best_excl_gain
+        
+        # Check if any rule has a negative gain. This should never happen, as we only add rules with positive gain
+        condition3 = len(excl_beam.gains) > 0 and len(incl_beam.gains) > 0 and np.max(incl_beam.gains) < 0 and np.max(excl_beam.gains) < 0
+        return (condition1 and condition2) or condition3
 
     def combine_beams(self, incl_beam_list, excl_beam_list):
-        """ Combine multiple beams TODO
+        """ Receives a list of beams, each containing the information from a grow step for a rule that's being grown
+        Finds the 
         
         Parameters
         ---
@@ -148,9 +219,45 @@ class Ruleset:
         final_info_incl : TODO
         final_info_excl : TODO
         """
-        pass
+        infos_incl, infos_excl = [], []
+        coverages_incl, coverages_excl = [], []
 
-    def search_next_rule(self, k_consecutively, rule_given=None):
+        # First, we combine the info and coverage lists for the incl beam
+        for incl_beam in incl_beam_list:
+            infos_incl.extend([info for info in incl_beam.infos.values() if info is not None])
+            coverages_incl.extend([info["coverage_incl"] for info in incl_beam.infos.values() if info is not None])
+        
+        # Argsort the coverage lists
+        # QUESTION: Why is this array_split here
+        argsorted_coverages_incl = np.argsort(coverages_incl)
+        groups_coverages_incl = np.array_split([infos_incl[i] for i in argsorted_coverages_incl], self.data_info.beam_width)
+
+        # Now we find the best info for each group
+        final_info_incl = []
+        for group in groups_coverages_incl:
+            if len(group) == 0:
+                continue
+            final_info_incl.append(group[np.argmax([info["normalized_gain_incl"] for info in group])])
+
+        # Repeat the process for the excl beam
+        for excl_beam in excl_beam_list:
+            infos_excl.extend([info for info in excl_beam.infos.values() if info is not None])
+            coverages_excl.extend([info["coverage_excl"] for info in excl_beam.infos.values() if info is not None])
+        argsorted_coverages_excl = np.argsort(coverages_excl)
+        groups_coverages_excl = np.array_split([infos_excl[i] for i in argsorted_coverages_excl], self.data_info.beam_width)
+        
+        final_info_excl = []
+        for group in groups_coverages_excl:
+            if len(group) == 0:
+                continue
+            final_info_excl.append(group[np.argmax([info["normalized_gain_excl"] for info in group])])
+        
+        return final_info_incl, final_info_excl
+
+    def search_next_rule(self, 
+                         k_consecutively, 
+                         rule_given: Rule.Rule = None
+                         ):
         """ Execute a beam search to find the next rule to add to the ruleset
         
         Parameters
@@ -165,4 +272,70 @@ class Ruleset:
           : Rule object
             Rule found by the beam search
         """
-        pass
+        if rule_given is None:
+            rule = Rule(indices=np.arange(self.data_info.nrow), 
+                        indices_excl=self.uncovered_indices,
+                        data_info=self.data_info, 
+                        rule_base=None,
+                        condition_matrix=np.repeat(np.nan, self.data_info.ncol * 2).reshape(2, self.data_info.ncol),
+                        ruleset=self, 
+                        mdl_gain=-np.Inf, 
+                        mdl_gain_excl=-np.Inf, 
+                        icols_in_order=[])
+        else:
+            rule = rule_given  
+
+        rules_for_next_iter = [rule]
+        rules_candidates = [rule]
+
+        previous_best_gain, previous_best_excl_gain = -np.Inf, -np.Inf
+        counter_worse_best_gain = 0
+
+        # TODO: Figure out what max_grow_iter is
+        for i in range(self.data_info.max_grow_iter):
+            excl_beam_list, incl_beam_list = [], []
+
+            # For each rule, initialize a beam and put information for a grow step in it
+            for rule in rules_for_next_iter:
+                excl_beam = Beam.DiverseCovBeam(width=self.data_info.beam_width)
+                incl_beam = Beam.DiverseCovBeam(width=self.data_info.beam_width)
+                rule.grow(grow_info_beam=incl_beam, grow_info_beam_excl=excl_beam)
+                excl_beam_list.append(excl_beam)
+                incl_beam_list.append(incl_beam)
+            
+            # Combine the beams and make GrowInfoBeam objects to store them
+            final_info_incl, final_info_excl = self.combine_beams(incl_beam_list, excl_beam_list)
+            final_incl_beam = Beam.GrowInfoBeam(width=self.data_info.beam_width)
+            final_excl_beam = Beam.GrowInfoBeam(width=self.data_info.beam_width)
+
+            for info in final_info_incl:
+                final_incl_beam.update(info, info["normalized_gain_incl"])
+            for info in final_info_excl:
+                final_excl_beam.update(info, info["normalized_gain_excl"])
+            
+            # If the beams are empty, stop the search
+            if len(final_incl_beam.gains) == 0 and len(final_excl_beam.gains) == 0:
+                break
+            
+            # If the stop condition is met, increment the counter
+            stop_condition_element = self.calculate_stop_condition_element(final_incl_beam, final_excl_beam, previous_best_gain, previous_best_excl_gain)
+            if stop_condition_element:
+                counter_worse_best_gain = counter_worse_best_gain + 1
+            else:
+                counter_worse_best_gain = 0
+
+            # If we found a rule with better gain, store it
+            if len(final_incl_beam.gains) > 0:
+                previous_best_gain = np.max(final_incl_beam.gains)
+            if len(final_excl_beam.gains) > 0:
+                previous_best_excl_gain = np.max(final_excl_beam.gains)
+
+            # Stop the search if we find no improvement in k_consecutively iterations
+            if counter_worse_best_gain > k_consecutively:
+                break
+            else:
+                rules_for_next_iter = extract_rules_from_beams([final_excl_beam, final_incl_beam])
+                rules_candidates.extend(rules_for_next_iter)
+
+        which_best_ = np.argmax([r.incl_gain_per_excl_coverage for r in rules_candidates])
+        return rules_candidates[which_best_]
